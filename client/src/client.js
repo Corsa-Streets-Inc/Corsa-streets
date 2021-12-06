24956b3f61dd36dcc4fa7df3e3ff44685ed54623
PavelOwO <u10a16@nvk45.kh.ua>
2021-12-06 19:23:23 +0200

    токийский дрифт

--- client/src/client.js
+++ client/src/client.js
@@ -17,25 +17,48 @@ const onChatSubmitted = (sock) => (e) => {
     sock.emit('message', text)
 }
 
-const draw = () => {
+const draw = (sock) => {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
-    ctx.beginPath();
-    ctx.rect(0,0,400,400);
-    ctx.fillStyle = "#00000F";
-    ctx.fill();
-    ctx.closePath(); 
-    ctx.beginPath();
-    ctx.rect(190, 175, 20, 50);
-    ctx.fillStyle = "#FF0000";
-    ctx.fill();
-    ctx.closePath();    
+    var x = -10, y = -25, a = 0; 
+
+    const drawCar = () => {
+        ctx.beginPath();
+        ctx.rect(0,0,400,400);
+        ctx.fillStyle = "#00000F";
+        ctx.fill();
+        ctx.closePath(); 
+        ctx.beginPath();
+        ctx.save();
+        ctx.translate(200, 200)
+        ctx.rotate(a);
+        ctx.rect(x, y, 20, 50);
+        ctx.fillStyle = "#FF0000";
+        ctx.fill();
+        ctx.restore();
+        ctx.closePath();    
+    }
+    document.addEventListener('keydown', (e) => {
+        if(e.keyCode == 39) {
+            a += Math.PI / 60;
+        }
+        else if (e.keyCode == 37) {
+            a -= Math.PI / 180;
+        }
+        else if (e.key == " ") {
+            x += 10;
+            y += 10;
+        }
+    })
+
+    setInterval(drawCar, 100)
+
 }
 
 
 (() => {
     const sock = io();
-    draw();
+    draw(sock);
     sock.on('message', log)
     document.querySelector("#chat-form").addEventListener("submit", onChatSubmitted(sock))
 })();
