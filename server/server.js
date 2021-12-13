b5516a60e35e15ed357ccea458d54817ceb67044
Dmytro Hai <darkfightik@gmail.com>
2021-12-13 18:54:19 +0200

    Чел заходит и получает машинку по айди

--- server/server.js
+++ server/server.js
@@ -8,12 +8,15 @@ app.use(express.static(`${__dirname}/../client`));
 
 const server = http.createServer(app);
 const io = socketio(server);
-
-
-
+let car = {
+    x:0,
+    y:0
+}
+var playerCars = new Map();
 
 io.on('connection', (sock) => {
     const id = Math.trunc(Math.random() * 100000);
+    playerCars.set(id,new car);
     io.emit('message', "player " + id + " has connected", id)
     // sock.emit('initCar')
     sock.on('message', (text) => io.emit('message', text, id))
