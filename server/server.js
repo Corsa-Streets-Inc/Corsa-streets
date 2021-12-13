6e62efb182a6a3cd5abc633047bbab204ddc32eb
Dmytro Hai <darkfightik@gmail.com>
2021-12-13 19:11:51 +0200

    Машина теперь класс!

--- server/server.js
+++ server/server.js
@@ -8,15 +8,20 @@ app.use(express.static(`${__dirname}/../client`));
 
 const server = http.createServer(app);
 const io = socketio(server);
-let car = {
-    x:0,
-    y:0
+
+class Car {
+    constructor(id) {
+        this.id = id;
+        this.x = 0;
+        this.y = 0;
+    }
 }
+
 var playerCars = new Map();
 
 io.on('connection', (sock) => {
     const id = Math.trunc(Math.random() * 100000);
-    playerCars.set(id,new car);
+    playerCars.set(id,new Car);
     io.emit('message', "player " + id + " has connected", id)
     // sock.emit('initCar')
     sock.on('message', (text) => io.emit('message', text, id))
@@ -26,6 +31,6 @@ server.on('error', (err) => {
     console.error(err);
 })
 
-server.listen(8080, '25.77.209.239', () => {
+server.listen(8080, 'localhost', () => {
     console.log("here we go!")
 })
